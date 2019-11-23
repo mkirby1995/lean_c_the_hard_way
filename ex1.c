6c6dd54441c3819406f0f26df5153307434329f7
Matt.Kirby <mkirby3@angelo.edu>
2019-11-23 16:26:05 -0600

    add 1-7

---
+++ ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    int b = 249;
+    int m = 4;
+    int x = 42;
+
+    //this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("M %d.\n", m * x + b);
+
+    return 0;
+}
